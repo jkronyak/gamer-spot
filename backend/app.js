65645d3af09b456011f1d930dfea22afd33e67d7
Jared Anthony Kronyak <jared.kronyak@gmail.com>
2023-03-23 16:14:27 -0400

    Changes to backend app.js for utilizing React production build

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,26 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 import configRoutes from './routes/index.js';
 
+if(process.env.ENVIRONMENT !== 'production') { 
+	dotenv.config();
+}
+
+const port = process.env.PORT || 4000;
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.get('/', (_, res) => { 
+	res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+});	
+
 configRoutes(app);
 
-app.listen(4000, () => { 
-	console.log("Server running on http://localhost:4000/")
+app.listen(port, () => { 
+	console.log(`Server running on http://localhost:${4000}/`)
 });
