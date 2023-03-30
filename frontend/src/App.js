7d7000ac2b3e7aa0b0bf135bef8b6a9719df1660
Jared Anthony Kronyak <56709613+jkronyak@users.noreply.github.com>
2023-03-30 14:44:58 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
 		<Router>
 			<Routes>
-				<Route path="/" element={<Home/>} />
+				<Route path="/home" element={<Home/>} />
 			</Routes>
 		</Router>
     </div>
