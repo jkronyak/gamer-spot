85a2c5340f9d94579c0ce4df755b540888309601
jkronyak <56709613+jkronyak@users.noreply.github.com>
2023-04-03 13:37:01 -0400

    testing GitHub actions for uploading .env file

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import axios from 'axios';
 import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -23,7 +24,21 @@ app.get('*', (_, res) => {
 	res.sendFile(path.join(__dirname, '../../../root/frontend/build/index.html'))
 });
 
+const generateTwitchToken = async () => { 
+	const clientId = process.env.TWITCH_CLIENT_ID;
+	const clientSecret = process.env.TWITCH_CLIENT_SECRET;
+	const url = `https://id.twitch.tv/oauth2/token?client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials`;
+	try {
+		const response = await axios.post(url);
+		const { access_token } = response.data;
+		process.env.TWITCH_AUTH_TOKEN = token;
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+
 
 app.listen(port, () => { 
-	console.log(`Server running on http://localhost:${4000}/`)
+	console.log(`Server running on http://localhost:${port}/`)
 });
