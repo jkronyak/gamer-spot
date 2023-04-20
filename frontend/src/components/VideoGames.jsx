72fdc7ec2578af03606d5c10554cb5ceeee60c84
Erik Anhorn <erikjanhorn@gmail.com>
2023-04-20 16:04:10 -0400

    Added small list of games to video games page

--- frontend/src/components/VideoGames.jsx
+++ frontend/src/components/VideoGames.jsx
@@ -1,3 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-export default function VideoGames() {}
+export default function VideoGames() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_BACKEND_URL}/games`
+      );
+      setGames(data);
+    };
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {games.map((game) => {
+          return (
+            <li key={game.id}>
+              <a href={`/games/${game.id}`}>{game.name}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
