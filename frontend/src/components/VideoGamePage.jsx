e2b2573fc4566ad36def0ad732b95287560723f1
Erik Anhorn <erikjanhorn@gmail.com>
2023-04-20 14:56:28 -0400

    Put breaks between similar game links

--- frontend/src/components/VideoGamePage.jsx
+++ frontend/src/components/VideoGamePage.jsx
@@ -2,53 +2,51 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
-
 const VideoGamePage = () => {
-    const { id } = useParams();
+  const { id } = useParams();
 
-    const [gameData, setGameData] = useState({});
+  const [gameData, setGameData] = useState({});
 
-    useEffect(() => { 
-        const fetchData = async () => { 
-            const { data } = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/games/${id}`);
-            setGameData(data);
-        }
-        fetchData();
-    }, [id]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_BACKEND_URL}/games/${id}`
+      );
+      setGameData(data);
+    };
+    fetchData();
+  }, [id]);
 
-    if(!gameData || Object.keys(gameData).length === 0) { 
-        return <h1>Loading...</h1> 
-    }
+  if (!gameData || Object.keys(gameData).length === 0) {
+    return <h1>Loading...</h1>;
+  }
 
-    return (
-        <div>
-            <h1>Video Game Page</h1>
-            <h2>{gameData.name}</h2>
-            <img src={gameData.coverUrl} alt={gameData.name} />
-            <p>Release Date: {gameData.release_date}</p>
-            <p>{gameData.summary}</p>
-            <p>Category: {gameData.category}</p>
-            <h3>Genres</h3>
-            {
-                gameData.genres.map(genre => (
-                    <p key={genre}>{genre}</p>
-                ))
-            }
-            <h3>Similar Games</h3>
-            {
-                gameData.similar_games.map( (game) => { 
-                    return <a href={`/games/${game.id}`}>{game.name}</a>
-                })
-            }
-            <h3>Screenshots</h3>
-            {
-                gameData.screenshots.map( (screenshot) => {
-                    return <img src={screenshot} alt={gameData.name}></img>
-               })
-            }
-
-        </div>
-    );
+  return (
+    <div>
+      <h1>Video Game Page</h1>
+      <h2>{gameData.name}</h2>
+      <img src={gameData.coverUrl} alt={gameData.name} />
+      <p>Release Date: {gameData.release_date}</p>
+      <p>{gameData.summary}</p>
+      <p>Category: {gameData.category}</p>
+      <h3>Genres</h3>
+      {gameData.genres.map((genre) => (
+        <p key={genre}>{genre}</p>
+      ))}
+      <h3>Similar Games</h3>
+      {gameData.similar_games.map((game) => {
+        return (
+          <div>
+            <a href={`/games/${game.id}`}>{game.name}</a>
+          </div>
+        );
+      })}
+      <h3>Screenshots</h3>
+      {gameData.screenshots.map((screenshot) => {
+        return <img src={screenshot} alt={gameData.name}></img>;
+      })}
+    </div>
+  );
 };
 
 export default VideoGamePage;
